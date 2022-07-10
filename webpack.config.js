687651dd6d333feb01e0938f31adc901f46cf999
sqguo <sqguo@uwaterloo.ca>
2022-07-10 02:38:53 -0400

    load scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = function (_env, argv) {
   const isProduction = argv.mode === "production";
 
   return {
     entry: "./src/index.tsx",
-    plugins: [new Dotenv()],
+    plugins: [
+      new Dotenv(),
+      isProduction &&
+        new MiniCssExtractPlugin({
+          filename: "assets/css/[name].[contenthash:8].css",
+          chunkFilename: "assets/css/[name].[contenthash:8].chunk.css",
+        }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        filename: "index.html",
+        inject: "body",
+      }),
+    ].filter(Boolean),
     module: {
       rules: [
         {
@@ -21,6 +34,35 @@ module.exports = function (_env, argv) {
           },
           exclude: /node_modules/,
         },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+            "sass",
+          ],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/i,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        },
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
       ],
     },
     resolve: {
@@ -29,6 +71,17 @@ module.exports = function (_env, argv) {
     output: {
       filename: "bundle.js",
       path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: 3000,
+      open: true,
+      client: {
+        overlay: true,
+      },
     },
   };
 };
